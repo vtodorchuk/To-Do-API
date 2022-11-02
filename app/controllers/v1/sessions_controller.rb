8c0a19ccea616bcaf42b8d4a1bb1e5dc06725d3a
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-02 17:46:55 +0200

    Fix Rubocop

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,21 +1,23 @@
-class V1::SessionsController < ApplicationController
-  def create
-    user = User.find_by(email: params[:email])
+module V1
+  class SessionsController < ApplicationController
+    def create
+      user = User.find_by(email: params[:email])
 
-    if user&.valid_password?(params[:password])
-      render json: user.to_json(only: [:id, :authentication_token]), status: :created
-    else
-      head(:unauthorized)
+      if user&.valid_password?(params[:password])
+        render json: user.to_json(only: %i[id authentication_token]), status: :created
+      else
+        head(:unauthorized)
+      end
     end
-  end
 
-  def destroy
-    current_user = User.find(params[:id])
+    def destroy
+      current_user = User.find(params[:id])
 
-    if current_user&.update(authentication_token: '')
-      render status: :ok
-    else
-      head(:unauthorized)
+      if current_user&.update(authentication_token: '')
+        render status: :ok
+      else
+        head(:unauthorized)
+      end
     end
   end
 end
