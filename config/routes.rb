69b4ec74c9600e112a5f1205f736ca97ade6b7be
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-04 11:41:13 +0200

    Remove unused routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  # devise_for :users
-
   namespace :v1 do
     post 'user/sing_in', to: 'sessions#create'
     post 'user/sing_up', to: 'users#create'
