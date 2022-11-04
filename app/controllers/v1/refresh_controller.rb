9152dc69c0313789893cf02134eb4e1be732978f
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-04 17:32:00 +0200

    Rebase

--- app/controllers/v1/refresh_controller.rb
+++ app/controllers/v1/refresh_controller.rb
@@ -3,7 +3,7 @@ module V1
     before_action :authorizauthorize_refresh_request!
 
     def create
-      session = JWTSession::Session.new(payload: payload, refresh_by_access_allowed: true)
+      session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
       token = session.refresh_by_access_allowed do
         raise JWTSessions::Errors::Unauthorized, I18n.t('session.errors.something_not_right')
       end
