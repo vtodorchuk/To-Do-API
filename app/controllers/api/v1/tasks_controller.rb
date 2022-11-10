bb2109fc4bc6dfa7a859cf5de7e6ee2e496fbc70
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-10 11:37:47 +0200

    Refactor

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,10 +3,10 @@ class Api::V1::TasksController < ApplicationController
 
   def index
     project = current_user.projects.find(params[:project_id])
-    tasks = project.tasks
+    @tasks = project.tasks
 
-    if tasks
-      render json: { data: { tasks: tasks.to_json } }, status: :found
+    if @tasks
+      render json: { data: { tasks: @tasks.to_json } }, status: :found
     else
       render status: :not_found
     end
@@ -14,40 +14,40 @@ class Api::V1::TasksController < ApplicationController
 
   def create
     project = current_user.projects.find(params[:project_id])
-    task = project.tasks.new(params)
+    @task = project.tasks.new(params)
 
-    if task.save
-      render json: { data: { task: task.to_json } }, status: :created
+    if @task.save
+      render json: { data: { task: @task.to_json } }, status: :created
     else
-      render json: { data: { errors: task.errors.full_messages } }, status: :unprocessable_entity
+      render json: { data: { errors: @task.errors.full_messages } }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    task = Task.where(id: params[:id]).first
+    @task = Task.where(id: params[:id]).first
 
-    if task&.destroy
+    if @task&.destroy
       render status: :ok
     else
-      render json: { data: { errors: task.errors.full_messages } }, status: :unprocessable_entity
+      render json: { data: { errors: @task.errors.full_messages } }, status: :unprocessable_entity
     end
   end
 
   def update
-    task = Task.where(id: params[:id]).first
+    @task = Task.where(id: params[:id]).first
 
     if task&.update(params[:task])
       render status: :ok
     else
-      render json: { data: { errors: task.errors.full_messages } }, status: :unprocessable_entity
+      render json: { data: { errors: @task.errors.full_messages } }, status: :unprocessable_entity
     end
   end
 
   def show
-    task = Task.where(id: params[:id]).first
+    @task = Task.where(id: params[:id]).first
 
-    return render status: :not_found if task.nil?
+    return render status: :not_found if @task.nil?
 
-    render json: { data: { task: task.to_json } }, status: :found
+    render json: { data: { task: @task.to_json } }, status: :found
   end
 end
