b557bdbd6f015140dffa3ff1ed7a9066561e3c54
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-10 11:38:24 +0200

    Add Project routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[create destroy]
       resources :refresh, only: :create
-
+      
       resource :session, only: %i[create destroy]
+
+      resources :projects
     end
   end
 end
