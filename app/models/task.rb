ba0c31626e2a0743ff6c84af1db3e06f6293f227
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-11 11:01:25 +0200

    Fix

--- app/models/task.rb
+++
@@ -1,9 +0,0 @@
-class Task < ApplicationRecord
-  belongs_to :project
-
-  validate :deadline_is_possible?
-
-  def deadline_is_possible?
-    errors.add(:deadline, 'must be possible') if deadline < Date.current
-  end
-end
