155c8ecd73bb71fbf3a3820eaf621a816362da40
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-11 11:43:30 +0200

    Add ActiveStorage

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,14 +2,13 @@ class Task < ApplicationRecord
   belongs_to :project
 
   has_many :comments, dependent: :destroy
+
   has_many_attached :files
 
   validates :deadline_is_possible?
 
   def deadline_is_possible?
-    current = Date.current
-
-    return unless deadline < current
+    return unless deadline < DateTime.now
 
     errors.add(:deadline, 'must be possible')
   end
