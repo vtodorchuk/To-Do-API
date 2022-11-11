74028c57a4681605024c3a2e6f078f8ae2a1cee2
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-11 16:12:16 +0200

    Fix

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   MAX_TEXT_LENGTH = 80
 
   belongs_to :user
+
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: MAX_TEXT_LENGTH }
