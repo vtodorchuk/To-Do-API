84eee31634d0ec31c77a649382ee42719f96dab3
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-17 16:44:17 +0200

    Refactor specs

--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -1,4 +1,4 @@
-describe Api::V1::ProjectsController do
+describe Api::V1::ProjectsController, api: true, type: :controller do
   include Docs::V1::Projects::Api
   let(:user) { create(:user) }
   let(:project) { create(:project, user: user) }
@@ -13,148 +13,112 @@ describe Api::V1::ProjectsController do
   end
 
   describe 'GET #index' do
+    include Docs::V1::Projects::Index
     context 'when success' do
-      include Docs::V1::Projects::Index
       before do
         project
         request.headers[JWTSessions.access_header] = access_token
-        get :index
       end
 
       it 'has status', :dox do
-        expect(response).to have_http_status(:found)
-      end
-
-      it 'get projects', :dox do
-        expect(JSON.parse(response.body)).to eq([{ 'id' => project.id,
-                                                   'name' => project.name,
-                                                   'user_id' => project.user.id }])
-      end
-    end
-
-    context 'when failure' do
-      before do
-        request.headers[JWTSessions.access_header] = access_token
         get :index
-      end
-
-      it do
         expect(response).to have_http_status(:found)
-      end
-
-      it do
-        expect(JSON.parse(response.body)).to be_empty
       end
     end
   end
 
   describe 'GET #show' do
+    include Docs::V1::Projects::Show
     context 'when success' do
-      include Docs::V1::Projects::Show
       before do
         project
         request.headers[JWTSessions.access_header] = access_token
-        get :show, params: { id: project.id }
       end
 
       it 'has status', :dox do
+        get :show, params: { id: project.id }
         expect(response).to have_http_status(:found)
-      end
-
-      it 'show project', :dox do
-        expect(JSON.parse(response.body)).to eq({ 'id' => project.id,
-                                                  'name' => project.name,
-                                                  'user_id' => project.user.id })
       end
     end
 
     context 'when failure' do
       before do
         request.headers[JWTSessions.access_header] = access_token
-        get :show, params: { id: rand(0..10) }
       end
 
-      it do
+      it 'has status', :dox do
+        get :show, params: { id: rand(0..10) }
         expect(response).to have_http_status(:not_found)
       end
     end
   end
 
   describe 'POST #create' do
+    include Docs::V1::Projects::Create
     context 'when success' do
-      include Docs::V1::Projects::Create
       before do
         request.headers[JWTSessions.access_header] = access_token
-        get :create, params: { name: new_name }
       end
 
       it 'has status', :dox do
+        get :create, params: { name: new_name }
         expect(response).to have_http_status(:created)
-      end
-
-      it 'add project', :dox do
-        expect(JSON.parse(response.body)['name']).to eq(new_name)
       end
     end
 
     context 'when failure' do
       before do
         request.headers[JWTSessions.access_header] = access_token
+      end
+
+      it 'has status', :dox do
         get :create
-      end
-
-      it do
         expect(response).to have_http_status(:unprocessable_entity)
-      end
-
-      it do
-        expect(JSON.parse(response.body)['errors']).to eq(['Name can\'t be blank'])
       end
     end
   end
 
   describe 'PUT #update' do
+    include Docs::V1::Projects::Update
     context 'when success' do
-      include Docs::V1::Projects::Update
       before do
         project
         request.headers[JWTSessions.access_header] = access_token
-        put :update, params: { id: project.id, name: new_name }
       end
 
       it 'has status', :dox do
+        put :update, params: { id: project.id, name: new_name }
         expect(response).to have_http_status(:ok)
       end
 
-      it 'update project', :dox do
-        expect(JSON.parse(response.body)).to eq({ 'id' => project.id,
-                                                  'name' => project.reload.name,
-                                                  'user_id' => project.user.id })
+      it 'update projects', :dox do
+        put :update, params: { id: project.id, name: new_name }
+        expect(project.reload.name).to eq(new_name)
       end
     end
 
     context 'when failure' do
       before do
         request.headers[JWTSessions.access_header] = access_token
-        put :update, params: { id: rand(0...12) }
       end
 
-      it do
+      it 'has status', :dox do
+        put :update, params: { id: rand(0...12) }
         expect(response).to have_http_status(:not_found)
       end
     end
   end
 
   describe 'DELETE #destroy' do
+    include Docs::V1::Projects::Destroy
     context 'when success' do
-      include Docs::V1::Projects::Destroy
       before do
         project
         request.headers[JWTSessions.access_header] = access_token
-        get :destroy, params: { id: project.id }
       end
 
       it 'has status', :dox do
+        get :destroy, params: { id: project.id }
         expect(response).to have_http_status(:ok)
       end
     end
@@ -162,10 +126,10 @@ describe Api::V1::ProjectsController do
     context 'when failure' do
       before do
         request.headers[JWTSessions.access_header] = access_token
-        get :destroy, params: { id: rand(0..10) }
       end
 
-      it do
+      it 'has status', :dox do
+        get :destroy, params: { id: rand(0..10) }
         expect(response).to have_http_status(:not_found)
       end
     end
