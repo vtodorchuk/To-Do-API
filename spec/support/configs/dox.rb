cbb45571c692fc2010c35c489f8c5facfe8cadc7
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-24 19:18:34 +0200

    Fix

--- spec/support/configs/dox.rb
+++ spec/support/configs/dox.rb
@@ -2,8 +2,6 @@ Dir[Rails.root.join('spec/docs/**/*.rb')].sort.each { |file| require file }
 
 Dox.configure do |config|
   config.descriptions_location = Rails.root.join('spec/docs/v1/descriptions')
-  config.schema_request_folder_path = Rails.root.join('spec/docs/v1/schemas')
-  config.schema_response_folder_path = Rails.root.join('spec/support/v1/schemas')
   config.title = 'API'
   config.headers_whitelist = %w[Accept Authorization]
   config.api_version = '1.0'
