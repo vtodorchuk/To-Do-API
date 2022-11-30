989927a3ad06be5df5ef81227b84bb3a1c512301
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-30 16:40:07 +0200

    fix

--- app/concepts/v1/project/operation/destroy.rb
+++ app/concepts/v1/project/operation/destroy.rb
@@ -3,7 +3,7 @@ class V1::Project::Operation::Destroy < Trailblazer::Operation
   step :destroy_project
 
   def find_project(ctx, params:, current_user:, **)
-    ctx[:project] = Project.find_by(id: params[:id], user_id: current_user.id)
+    ctx[:project] = current_user.projects.find_by(id: params[:id])
   end
 
   def destroy_project(_ctx, project:, **)
