13abe61571b6859d3fc3898ba0aab69990d44f79
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-30 16:44:17 +0200

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[create destroy]
       resources :refresh, only: :create
-      
+
       resource :session, only: %i[create destroy]
 
       resources :projects
