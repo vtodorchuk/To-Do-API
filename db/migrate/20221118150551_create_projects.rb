5f6c128967a919da4ff6d7697382f8a7c251cb85
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-30 10:17:31 +0200

    Add index to project name

--- db/migrate/20221118150551_create_projects.rb
+++ db/migrate/20221118150551_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[6.1]
   def change
     create_table :projects do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :name
+      t.string :name, index: { unique: true, name: 'unique_project_name' }
 
       t.timestamps
     end
